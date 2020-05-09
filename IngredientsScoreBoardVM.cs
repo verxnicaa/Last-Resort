1f23f47cb0d7c9f4556df7e7bcc08dc8ed8467f4
verxnicaa <65091553+verxnicaa@users.noreply.github.com>
2020-05-09 16:48:41 -0400

    Add files via upload

---
+++ IngredientsScoreBoardVM.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IngredientsScoreBoardVM : MonoBehaviour
+{
+    [SerializeField] int ingredientsRemaining = 5;
+    public Text ingredientsRemainingText;
+    int scoreDecrease = 1;
+
+    private PlayerScriptVM playerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ingredientsRemainingText = GetComponent<Text>();
+        ingredientsRemainingText.text = ingredientsRemaining.ToString();
+
+        playerScript = GameObject.FindObjectOfType<PlayerScriptVM>();
+    }
+
+    public void Collected(int scoreDecrease)
+    {
+        ingredientsRemaining = ingredientsRemaining - 1;
+        ingredientsRemainingText.text = ingredientsRemaining.ToString();
+
+        playerScript.UpdateChecklist(ingredientsRemaining);
+    }
+}
---
+++ PlayerScriptVM.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerScriptVM : MonoBehaviour
+{
+    public Text announcement;
+
+    int allIngredientsCollected = 1;
+    int allEnemiesDefeated = 1;
+    SceneLoaderVM sceneLoader;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneLoader = GetComponent<SceneLoaderVM>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Stairs"))
+        {
+            if (allIngredientsCollected == 0)
+            {
+                ResetText();
+                SceneManager.LoadScene("Level 3 Victory");
+
+            }
+            if (allIngredientsCollected != 0)
+            {
+                announcement.text = "COLLECT ALL SPECIAL INGREDIENTS AND KILL ALL ZOMBIES TO CONTINUE";
+                Invoke("ResetText", 5f);
+            }
+
+        }
+    }
+
+    void ResetText()
+    {
+        announcement.text = "";
+    }
+
+    public void UpdateChecklist(int ingredientsRemaining)
+    {
+        if (ingredientsRemaining == 0)
+        {
+            allIngredientsCollected = ingredientsRemaining;
+        }
+    }
+
+    public void UpdateChecklistFinal(int enemiesRemaining)
+    {
+        if (enemiesRemaining == 0)
+        {
+            allEnemiesDefeated = enemiesRemaining;
+        }
+    }
+}
